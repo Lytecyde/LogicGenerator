d5383479cb216fa596ae4c9f75fb282a3f17bea6
Mik Seljamaa <Lytecyde@users.noreply.github.com>
2016-03-07 02:19:19 +0200

    First attempt.

---
+++ src/src/LogicGenerator.java
@@ -0,0 +1,19 @@
+/**
+ * Created by miku on 7.03.2016.
+ */
+public class LogicGenerator {
+    private final String AND =  "&&";
+    private final String OR = "||";
+    private final String NOTEQUAL =  "!=";
+    private final String EQUAL = "==";
+    public static void main(String[] args) {
+            int sentencesCount = 4;
+            sentencesCount = (int)Math.floor(Math.random()*17);
+
+    
+
+    }
+
+
+
+}
---
+++ src/src/Statement.java
@@ -0,0 +1,14 @@
+/**
+ * Created by miku on 7.03.2016.
+ */
+public class Statement {
+    boolean isCombo = false;
+    boolean isNotted = false;
+    String firstPremise = "";
+    String secondPremise = "";
+    int operator = 1;
+
+    public Statement() {
+
+    }
+}
